d0b79b1ebcc24c03b949ef64e4614b954faf1406
TikoHassan <tarekhassan912260@yahoo.com>
2017-04-15 21:49:55 +0200

    Created Project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            // fe el changes ht2lee el changes mn s3at 25er mara 3mlt commit aw sync 
+            // changes include enk 3mlt add le files aw delete le files w 3'ryt fe files
+            // in case of ey text files tt2dr tshooof el changes haweerk delw2ti
+        }
+    }
+}
