f9a910c5ccc2d474c9498fb15ba18523be1d560b
KarimAshraf1995 <karimashraf1995@gmail.com>
2017-04-15 22:12:05 +0200

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Test");
+            Console.WriteLine("Test2");
             // fe el changes ht2lee el changes mn s3at 25er mara 3mlt commit aw sync 
             // changes include enk 3mlt add le files aw delete le files w 3'ryt fe files
             // in case of ey text files tt2dr tshooof el changes haweerk delw2ti
